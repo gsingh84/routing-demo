ca13491c238236b8307565cc2bda6a8055d5f592
gsingh84 <gsingh84@mail.greenriver.edu>
2018-01-12 17:47:06 -0500

    added additional routes

--- index.php
+++ index.php
@@ -5,9 +5,22 @@ require_once('vendor/autoload.php');
 //Create an instance of the base class
 $f3 = Base::instance();
 
+//Set debug level
+$f3->set('DEBUG', 3);
+
 //Define a default route
 $f3->route('GET /', function() {
     echo "<h1>Routing Demo</h1>";
+});
+
+//Define a route for page 1
+$f3->route('GET /page1', function() {
+    echo "<h1>This is page 1</h1>";
+});
+
+//Define a route for page 1 subpage
+$f3->route('GET /page1/subpage-a', function() {
+    echo "<h1>This is page 1, Subpage A</h1>";
 });
 
 //Run fat free
